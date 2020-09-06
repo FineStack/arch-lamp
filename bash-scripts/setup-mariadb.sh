e2b2b0ff64d40199a91582d14a0bb313bbba27a5
Niladri Shekhar Dutta <39089565+FineStack@users.noreply.github.com>
2020-09-06 13:53:28 +0530

    Update setup-mariadb.sh

--- bash-scripts/setup-mariadb.sh
+++ bash-scripts/setup-mariadb.sh
@@ -6,7 +6,7 @@ read username
 printf "Enter new password for MariaDB: - "
 read password
 Q1="CREATE USER '$username'@'localhost' IDENTIFIED BY '$password';"
-Q2="GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost';"
+Q2="GRANT ALL PRIVILEGES ON *.* TO '$username'@'localhost' WITH GRANT OPTION;"
 Q3="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}"
 mysql -e "$SQL"
