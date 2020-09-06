f4e626fba99a69a450ae5b43b5f82f23018fd9d5
Niladri Shekhar Dutta <39089565+FineStack@users.noreply.github.com>
2020-09-06 15:36:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,7 @@ python3 python-scripts/enable-proxy.py
 sh bash-scripts/setup-mariadb.sh
 clear
 python3 python-scripts/setup-phpmyadmin.py
+python3 python-scripts/fix-phpmyadmin-errors.py
+clear
 sh bash-scripts/systemd.sh
 clear
