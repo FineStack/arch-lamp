d41c44df1abc2e68ac2cb42159dd1b55aef02d28
Niladri Shekhar Dutta <39089565+FineStack@users.noreply.github.com>
2020-09-06 15:35:51 +0530

    Add files via upload

---
+++ python-scripts/fix-phpmyadmin-errors.py
@@ -0,0 +1,13 @@
+blowfish = input('Enter the blowfish from the website: - ')
+
+with open('/etc/webapps/phpmyadmin/config.inc.php') as file:
+    text = file.read()
+
+text = text.replace("$cfg['blowfish_secret'] = ''",
+                    "$cfg['blowfish_secret'] = '" + blowfish + "'")
+
+with open('/etc/webapps/phpmyadmin/config.inc.php', 'w') as file:
+    file.write(text)
+
+with open('/etc/webapps/phpmyadmin/config.inc.php', 'a') as file:
+    file.write("\n$cfg['TempDir'] = '/tmp';")
